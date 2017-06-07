bfab9dc1109c50428f212f1b6b2418b80edf1ad3
Kyle Smith <kylesmith2@gmail.com>
2017-06-07 16:59:45 -0700

    speed up overlapping segment resolution using a point dictionary.

--- kicad_to_femm/fec.py
+++ kicad_to_femm/fec.py
@@ -4,8 +4,89 @@ FEMM *.fec file writing
 Provides abstraction to the FEC file sections to allow adding/removing geometry, and then outputting the file.
 """
 import gc
-import shapely.geometry as gm
+from collections import defaultdict
 from weakref import WeakKeyDictionary, ref, WeakSet
+from math import floor, ceil
+
+from kicad_to_femm.spinner import spinner
+
+# A small distance, points within this distance of each other should be considered a single point.
+small_distance = 1e-3
+
+
+def bresenham(segment, grid_size=1.0):
+    """ Yield integer coordinates for every box painted along the segment according to bresenham's algorithm.
+
+        The boxes have side length grid_size.
+        No point on the line will be more than 0.5 * grid_size away from a box in the x or y directions.
+    """
+    x0, y0 = segment.points[0].x / grid_size, segment.points[0].y / grid_size
+    x1, y1 = segment.points[1].x / grid_size, segment.points[1].y / grid_size
+
+    if abs(x1 - x0) > abs(y1 - y0):
+        flip_xy = 1
+    else:
+        flip_xy = -1
+        x0, y0 = y0, x0
+        x1, y1 = y1, x1
+
+    if x1 >= x0:
+        x_sign = 1
+    else:
+        x_sign = -1
+        x0 = -x0 + 1
+        x1 = -x1 + 1
+
+    if y1 >= y0:
+        y_sign = 1
+    else:
+        y_sign = -1
+        y0 = -y0 + 1
+        y1 = -y1 + 1
+
+    if x1 - x0:
+        d_error = (y1 - y0) / (x1 - x0)
+    else:
+        d_error = 0
+    y = d_error * (floor(x0) + 0.5 - x0) + y0
+    y, error = floor(y), y - floor(y)
+
+    for x in range(floor(x0), ceil(x1)):
+        yield [x * x_sign, y * y_sign][::flip_xy]
+        error += d_error
+        if error >= 1:
+            y += 1
+            error -= 1
+
+
+def distance(segment, point):
+    """ Return the distance from a segment to a point """
+    x0, y0 = point.x, point.y
+    x1, y1 = segment.points[0].x, segment.points[0].y
+    x2, y2 = segment.points[1].x, segment.points[1].y
+
+    # ax + by + c = 0
+    if x1 == x2:  # if vertical line
+        a = 1
+        b = 0
+        c = -x1
+    elif y1 == y2:  # if horizontal line
+        a = 0
+        b = 1
+        c = -y1
+    else:
+        a = 1 / (x2 - x1)
+        b = -1 / (y2 - y1)
+        c = -a * x1 - b * y1
+
+    # Calculate point F (closest point on line to point)
+    xf = (b * (b * x0 - a * y0) - a * c) / (a ** 2 + b ** 2)
+    yf = (a * (-b * x0 + a * y0) - b * c) / (a ** 2 + b ** 2)
+
+    if min(x1, x2) <= xf <= max(x1, x2) and min(y1, y2) <= yf <= max(y1, y2):
+        return ((xf-x0)**2+(yf-y0)**2)**0.5
+    else:
+        return min(((x1-x0)**2+(y1-y0)**2)**0.5, ((x2-x0)**2+(y2-y0)**2)**0.5)
 
 
 class Hashable:
@@ -31,7 +112,7 @@ class Multiton(Hashable, Sortable):
 
         Subclass may:
             -check if it has already been initialized when __init__ is called using the 'initialized' flag.
-                if it has, it can merge the new arguments into the existing instance.
+                if it has, new arguments can be merged into the existing instance.
             -provide a non-zero start index
     """
     START_INDEX = 0
@@ -49,6 +130,8 @@ class Multiton(Hashable, Sortable):
     @classmethod
     def indexed(cls):
         """ Return a list of all instances indexed by their keys. """
+        # Force garbage collection to ensure there are no orphaned objects left in the weak dictionary
+        gc.collect()
         sorted_instances = sorted(cls.instances, key=lambda instance: instance.__key__)
         for i, inst in enumerate(sorted_instances):
             inst.i = i + cls.START_INDEX
@@ -173,7 +256,7 @@ class Conductor(Multiton):
 
 
 class Point(Multiton):
-    """ Point is a multiton, but points are put into 1e-3mm (1um) square bins. """
+    """ Point is a multiton, but points are put into small square bins, rather than being unique for every location. """
     instances = WeakKeyDictionary()  # Strong reference held by the segment that contains it
     START_INDEX = 0  # Points are 0-indexed
 
@@ -198,7 +281,8 @@ class Point(Multiton):
     @property
     def __key__(self):
         if not self._cached_key:
-            self._cached_key = round(self.x * 1e3 + Point.X_GRID_OFF), round(self.y * 1e3 + Point.Y_GRID_OFF)
+            self._cached_key = (round(self.x / small_distance + Point.X_GRID_OFF),
+                                round(self.y / small_distance + Point.Y_GRID_OFF))
         return self._cached_key
 
     def __str__(self):
@@ -206,7 +290,7 @@ class Point(Multiton):
 
 
 class Segment(Multiton):
-    """ Segment is a multiton with strong references (must explicitly remove segments from the instance dict). """
+    """ Segment is a multiton with strong references (must explicitly remove segments from the instances dict). """
     instances = {}
 
     def __init__(self, points, conductor=None, boundary=None, mesh_size=-1):
@@ -337,9 +421,10 @@ class Block(Multiton):
         return '{}\t{}\t{}\t-1\t0\t0\n'.format(self.x, self.y, self.block_property.i)
 
 
+@spinner('FEC post-process: Merging close points... ')
 def _merge_close_points():
-    """ Shift the point grid around by 0.5 in all directions to collect points that are close, but just barely in
-        different grid cells.
+    """ Shift the point grid around by 0.5 in all directions to collect points that are close but just barely in
+        different grid boxes.
     """
     # Loop over possible grid offsets
     for i, offset in enumerate([(0, 0.5), (0.5, 0.5), (0.5, 0)]):
@@ -360,32 +445,47 @@ def _merge_close_points():
             segment.points = [new_point[point] for point in segment.points]
 
 
-def _resolve_overlaping_segments():
+@spinner('FEC post-process: Resolving overlapping segments... ')
+def _resolve_overlapping_segments():
     """ Process segments to remove overlaps.
 
         Assumes that no segments cross in the middle, since this should not be possible after the converter processing.
-
-        First splits segments in two wherever they are very nearly touching a point.
-        No new points are created, the endpoints will then be the point they almost touched.
-        This may result in overlapping segments.
+        Splits segments into two new segments wherever they are very nearly touching a point.
+        No new points are created.
     """
-    # Get a list of all points
-    points = list(Point.instances.keys())
+    # Size the grid to cover the entire area with a small enough size that any single box contains very few points
+    # but large enough that there are not too many boxes along any segment.
+    grid_size = 0.1
+
+    # Must also be at least twice as large as the 'small_distance' so that every point within 'small_distance' of a
+    # segment is guaranteed to be found when enumerating the grid boxes along the segment.
+    assert grid_size > 2 * small_distance
+
+    # Place each point into its grid box, as will as the surrounding 8.
+    point_map = defaultdict(set)
+    for point in Point.instances:
+        x_base, y_base = int(point.x // grid_size), int(point.y // grid_size)
+        for x in range(x_base-1, x_base+2):
+            for y in range(y_base - 1, y_base + 2):
+                point_map[(x, y)].add(point)
 
     # Get a list of all segments, then clear the segment dictionary
     segments_to_process = list(Segment.instances)
     Segment.instances = {}
 
-    # Pop segments and check for intersections until none left to process.
+    # Pop segments and check for point intersections until none are left to process.
     while segments_to_process:
         segment = segments_to_process.pop()
 
-        line = gm.LineString([(p.x, p.y) for p in segment.points])
+        # Test points from all grid boxes the segment passes through
+        split_points = set()
+        for grid_box in bresenham(segment, grid_size):
+            split_points |= point_map[tuple(grid_box)]
+
         unbroken = True
-        for split_point in points:
-            if (gm.Point(split_point.x, split_point.y).distance(line) < 1e-3) and (split_point not in segment.points):
-                print('split!')
-                # Line broken by point, and the point is not one of it's endpoints.
+        for split_point in split_points:
+            if (split_point not in segment.points) and (distance(segment, split_point) < small_distance):
+                # Line broken by a point, and the point is not one of it's endpoints.
                 # Split into 2 segments and add each to the list to process.
                 for end_points in [(segment.points[0], split_point), (split_point, segment.points[1])]:
                     new_seg = Segment(end_points, segment.conductor, segment.boundary, segment.mesh_size)
@@ -393,11 +493,11 @@ def _resolve_overlaping_segments():
                     segments_to_process.append(new_seg)
                 unbroken = False
                 break
-
         if unbroken:
             Segment.instances[segment] = ref(segment)
 
 
+@spinner('FEC post-process: Removing zero length segments... ')
 def _remove_zero_length_segments():
     """ Remove all segments whose start and end points are equal (zero length). """
     for segment in list(Segment.instances):
@@ -434,11 +534,8 @@ def write_out(file_name, precision=1e-8, frequency=0, min_angle=25, thickness=.0
 
         # Geometry post-processing functions
         _merge_close_points()
-        _resolve_overlaping_segments()
+        _resolve_overlapping_segments()
         _remove_zero_length_segments()
-
-        # Force garbage collection to ensure there are no orphaned objects in the weak dictionaries
-        gc.collect()
 
         # Write boundaries
         boundaries = Boundary.indexed()
